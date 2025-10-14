4b24eb1f395474a9360558bbe394587e7c991f8d
braninpodolski <marshallbranin@gmail.com>
2025-10-14 17:18:13 -0400

    Updated test file

--- benchmarks/test.c
+++ benchmarks/test.c
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 #include <ucontext.h>
 #include "../thread-worker.h"
-#include "../runqueue.c"
+// #include "../runqueue.c"
 
 /* A scratch program template on which to call and
  * test thread-worker library functions as you implement
@@ -17,26 +17,29 @@
  * This will not be graded.
  */
 
-void *test_func(void *arg) {
+void *test_func(void *arg)
+{
     printf("Test thread executing.\n");
     return NULL;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
-	/* Implement HERE */
-	worker_t thread1;
+    /* Implement HERE */
+    worker_t thread1;
     worker_t thread2;
     worker_t thread3;
     worker_create(&thread1, NULL, &test_func, NULL);
     worker_create(&thread2, NULL, &test_func, NULL);
     worker_create(&thread3, NULL, &test_func, NULL);
-    // worker_exit(&thread1);
-    // worker_exit(&thread2);
-    // worker_exit(&thread3);
+    worker_exit(&thread1);
+    worker_exit(&thread2);
+    worker_exit(&thread3);
+    worker_exit(&thread1);
 
-    Runqueue* q;
-    init_runqueue(&q);
+    // Runqueue* q;
+    // init_runqueue(&q);
 
-	return 0;
+    return 0;
 }
